d6a0af2c7cfaf71c9b11440f8de31cb4b8b34d53
Luke Bainbridge <lbainbridge@echidna.ca>
2018-08-22 16:59:06 -0400

    Install NPM and backstop.

--- bin/set-up-globals.sh
+++ bin/set-up-globals.sh
@@ -3,10 +3,17 @@
 set -ex
 
 apt-get update
+apt-get install -y gpg
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+apt-get install -y nodejs
 
+npm config set user 0
+npm config set unsafe-perm true
+npm install -g backstopjs
 composer global require -n "hirak/prestissimo:^0.3"
 
 /usr/bin/env COMPOSER_BIN_DIR=$HOME/bin composer --working-dir=$HOME require pantheon-systems/terminus "^1"
+
 
 echo 'export PATH=$PATH:$HOME/bin:$HOME/terminus/bin' >> $BASH_ENV
 echo 'export CIRCLE_ENV=ci-$CIRCLE_BUILD_NUM' >> $BASH_ENV
@@ -15,6 +22,4 @@ source $BASH_ENV
 set +ex
 
 mkdir $HOME/.ssh
-eval $(ssh-agent -s)
-ssh-add <(echo $SSH_PRIVATE_KEY | base64 --decode)
 echo "StrictHostKeyChecking no" >> "$HOME/.ssh/config"
