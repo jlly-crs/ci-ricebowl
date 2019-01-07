e00fd6adc72bc8969f80fc5b20963cd3fce1c25c
Hugo Pinheiro <hpinheiro@echidna.ca>
2019-01-07 16:39:56 -0500

    Add slack notification modifications drupal version

--- bin/slack_notify.php
+++ bin/slack_notify.php
@@ -13,6 +13,10 @@ require __DIR__ . '/../vendor/autoload.php';
  * Contains the slack notification helper.
  */
 
+// Load environment variables
+$cli_user = 'Drupal@echidna.ca';
+$environment = 'CRSStore';
+
 // Load Slack helper functions.
 require_once __DIR__ . '/slack_helper.php';
 
@@ -20,60 +24,52 @@ require_once __DIR__ . '/slack_helper.php';
 $slack_type = $argv[1];
 $slack_channel = getenv('SLACK_CHANNEL');
 
-switch ($slack_type) {
-  case 'wordpress_updates':
-    $slack_agent = 'Wordpress Update Manager';
-    $slack_icon = 'https://s.w.org/style/images/about/WordPress-logotype-wmark.png';
+switch($slack_type) {
+  case 'drupal_updates':
+    $slack_agent = 'Drupal Update Manager';
+    $slack_icon = 'http://live-drupalcon-github-magic.pantheonsite.io/sites/default/files/icons/drupal.png';
     $slack_color = '#0678BE';
-    $slack_message = 'Kicking off checks for updates for Wordpress core and contrib plugins...';
-    _slack_tell($slack_message, $slack_channel, $slack_agent, $slack_icon, $slack_color);
+    $slack_message = 'Kicking off checks for updates for Drupal core and contrib modules...';
+    _slack_tell( $slack_message, $slack_channel, $slack_agent, $slack_icon, $slack_color);
     break;
-
-  case 'wordpress_no_coreupdates':
-    $slack_agent = 'Wordpress Update Manager';
-    $slack_icon = 'https://s.w.org/style/images/about/WordPress-logotype-wmark.png';
+  case 'drupal_no_coreupdates':
+		$slack_agent = 'Drupal Update Manager';
+		$slack_icon = 'http://live-drupalcon-github-magic.pantheonsite.io/sites/default/files/icons/drupal.png';
+		$slack_color = '#0678BE';
+		$slack_message = array('Drupal core is up to date.');
+		_slack_tell( $slack_message, $slack_channel, $slack_agent, $slack_icon, $slack_color);
+		break;
+  case 'drupal_coreupdates':
+		$slack_agent = 'Drupal Update Manager';
+		$slack_icon = 'http://live-drupalcon-github-magic.pantheonsite.io/sites/default/files/icons/drupal.png';
+		$slack_color = '#0678BE';
+		$slack_message = array('Drupal core has an update *available*: ' . str_replace('Update to ', '', str_replace('\n', '', shell_exec('terminus upstream:updates:list ${SITE_UUID}.${TERMINUS_ENV} --field=message'))));
+		_slack_tell( $slack_message, $slack_channel, $slack_agent, $slack_icon, $slack_color);
+		break;
+  case 'drupal_no_moduleupdates':
+		$slack_agent = 'Drupal Update Manager';
+		$slack_icon = 'http://live-drupalcon-github-magic.pantheonsite.io/sites/default/files/icons/drupal.png';
+		$slack_color = '#0678BE';
+		$slack_message = array('Drupal contrib is up to date.');
+		_slack_tell( $slack_message, $slack_channel, $slack_agent, $slack_icon, $slack_color);
+		break;
+  case 'drupal_moduleupdates':
+    $slack_agent = 'Drupal Update Manager';
+    $slack_icon = 'http://live-drupalcon-github-magic.pantheonsite.io/sites/default/files/icons/drupal.png';
     $slack_color = '#0678BE';
-    $slack_message = array('Wordpress core is up to date.');
-    _slack_tell($slack_message, $slack_channel, $slack_agent, $slack_icon, $slack_color);
+    $updates = implode(", ", array_slice($argv,2));
+    $slack_message = array('Drupal contrib has *updates available* for the following modules: ' . $updates);
+    _slack_tell( $slack_message, $slack_channel, $slack_agent, $slack_icon, $slack_color);
     break;
-
-  case 'wordpress_coreupdates':
-    $slack_agent = 'Wordpress Update Manager';
-    $slack_icon = 'https://s.w.org/style/images/about/WordPress-logotype-wmark.png';
-    $slack_color = '#0678BE';
-    $slack_message = array(
-      'Wordpress core has an update *available*: ' . str_replace('Update to ', '', str_replace('\n', '', shell_exec('terminus upstream:updates:list ${SITE_UUID}.${TERMINUS_ENV} --field=message'))),
-    );
-    _slack_tell($slack_message, $slack_channel, $slack_agent, $slack_icon, $slack_color);
-    break;
-
-  case 'wordpress_no_pluginupdates':
-    $slack_agent = 'Wordpress Update Manager';
-    $slack_icon = 'https://s.w.org/style/images/about/WordPress-logotype-wmark.png';
-    $slack_color = '#0678BE';
-    $slack_message = array('Wordpress contrib is up to date.');
-    _slack_tell($slack_message, $slack_channel, $slack_agent, $slack_icon, $slack_color);
-    break;
-
-  case 'wordpress_pluginupdates':
-    $slack_agent = 'Wordpress Update Manager';
-    $slack_icon = 'https://s.w.org/style/images/about/WordPress-logotype-wmark.png';
-    $slack_color = '#0678BE';
-    $updates = implode(", ", array_slice($argv, 2));
-    $slack_message = array('Wordpress contrib has *updates available* for the following plugins: ' . $updates);
-    _slack_tell($slack_message, $slack_channel, $slack_agent, $slack_icon, $slack_color);
-    break;
-
   case 'visual_same':
     $slack_agent = 'BackstopJS Visual Regression';
     $slack_icon = 'https://garris.github.io/BackstopJS/assets/lemurFace.png';
     $slack_color = '#800080';
     $slack_message = array('No Visual Differences Detected!');
-    _slack_tell($slack_message, $slack_channel, $slack_agent, $slack_icon, $slack_color);
+    _slack_tell( $slack_message, $slack_channel, $slack_agent, $slack_icon, $slack_color); 
     break;
-
   case 'visual_different':
-    // Post the File Using Uploads.IM.
+    // Post the File Using Uploads.IM
     $file_name_with_full_path = $argv[2];
     $response = \Cloudinary\Uploader::upload($file_name_with_full_path);
 
@@ -97,11 +93,11 @@ switch ($slack_type) {
     $slack_icon = 'https://circleci.com/circleci-logo-stacked-fb.png';
     $slack_color = '#229922';
     $slack_message = 'Time to check for new updates! Kicking off a new build...';
-    _slack_tell($slack_message, $slack_channel, $slack_agent, $slack_icon, $slack_color);
+    _slack_tell( $slack_message, $slack_channel, $slack_agent, $slack_icon, $slack_color);
     $slack_message = array();
     $slack_message['Build ID'] = $argv[2];
-    $slack_message['Build URL'] = 'https://circleci.com/gh/jlly-crs/ci-ricebowl/' . $argv[2];
-    _slack_tell($slack_message, $slack_channel, $slack_agent, $slack_icon, $slack_color);
+    $slack_message['Build URL'] = 'https://circleci.com/gh/Opswatch/ci-ricebowl/' . $argv[2];
+    _slack_tell( $slack_message, $slack_channel, $slack_agent, $slack_icon, $slack_color);
     break;
 
   case 'terminus_login':
@@ -109,35 +105,35 @@ switch ($slack_type) {
     $slack_icon = 'https://d1qb2nb5cznatu.cloudfront.net/startups/i/907-cbdae2927a54d2281280f41e954e8a3d-medium_jpg.jpg';
     $slack_color = '#1ec503';
     $slack_message = "Authenticating to Pantheon with Terminus machine token...";
-    _slack_tell($slack_message, $slack_channel, $slack_agent, $slack_icon, $slack_color);
+    _slack_tell( $slack_message, $slack_channel, $slack_agent, $slack_icon, $slack_color);
     $slack_message = array();
     $slack_message['CLI Version'] = '1.8.1';
-    $slack_message['CLI User'] = 'drupal@echidna.ca';
-    _slack_tell($slack_message, $slack_channel, $slack_agent, $slack_icon, $slack_color);
+    $slack_message['CLI User'] = $cli_user;
+    _slack_tell( $slack_message, $slack_channel, $slack_agent, $slack_icon, $slack_color);
     break;
 
   case 'terminus_coreupdates':
     $slack_agent = 'Terminus';
     $slack_icon = 'https://d1qb2nb5cznatu.cloudfront.net/startups/i/907-cbdae2927a54d2281280f41e954e8a3d-medium_jpg.jpg';
     $slack_color = '#1ec503';
-    $slack_message = 'Applying update for Wordpress core...';
-    _slack_tell($slack_message, $slack_channel, $slack_agent, $slack_icon, $slack_color);
-    $slack_message = array();
-    $slack_message['Operation'] = 'terminus upstream:updates:apply';
-    $slack_message['Environment'] = '`ci-update`';
-    _slack_tell($slack_message, $slack_channel, $slack_agent, $slack_icon, $slack_color);
+    $slack_message = 'Applying update for Drupal core...';
+    _slack_tell( $slack_message, $slack_channel, $slack_agent, $slack_icon, $slack_color);
+		$slack_message = array();
+		$slack_message['Operation'] = 'terminus upstream:updates:apply';
+		$slack_message['Environment'] = $environment;
+		_slack_tell( $slack_message, $slack_channel, $slack_agent, $slack_icon, $slack_color);
     break;
 
-  case 'terminus_pluginupdates':
+  case 'terminus_moduleupdates':
     $slack_agent = 'Terminus';
     $slack_icon = 'https://d1qb2nb5cznatu.cloudfront.net/startups/i/907-cbdae2927a54d2281280f41e954e8a3d-medium_jpg.jpg';
     $slack_color = '#1ec503';
-    $slack_message = "Applying updates for Wordpress contrib plugins...";
-    _slack_tell($slack_message, $slack_channel, $slack_agent, $slack_icon, $slack_color);
+    $slack_message = "Applying updates for Drupal contrib modules...";
+    _slack_tell( $slack_message, $slack_channel, $slack_agent, $slack_icon, $slack_color);
     $slack_message = array();
-    $slack_message['Operation'] = 'terminus wp plugin update --all';
-    $slack_message['Environment'] = '`ci-update`';
-    _slack_tell($slack_message, $slack_channel, $slack_agent, $slack_icon, $slack_color);
+    $slack_message['Operation'] = 'terminus drush pm-updatecode';
+    $slack_message['Environment'] = $environment;
+    _slack_tell( $slack_message, $slack_channel, $slack_agent, $slack_icon, $slack_color);
     break;
 
   case 'pantheon_multidev_setup':
@@ -145,11 +141,11 @@ switch ($slack_type) {
     $slack_icon = 'https://d1qb2nb5cznatu.cloudfront.net/startups/i/907-cbdae2927a54d2281280f41e954e8a3d-medium_jpg.jpg';
     $slack_color = '#1ec503';
     $slack_message = "Setting up a testing environment with Pantheon Multidev...";
-    _slack_tell($slack_message, $slack_channel, $slack_agent, $slack_icon, $slack_color);
+    _slack_tell( $slack_message, $slack_channel, $slack_agent, $slack_icon, $slack_color);
     $slack_message = array();
     $slack_message['Operation'] = 'terminus multidev:create';
-    $slack_message['Environment'] = '`ci-update`';
-    _slack_tell($slack_message, $slack_channel, $slack_agent, $slack_icon, $slack_color);
+    $slack_message['Environment'] = $environment;
+    _slack_tell( $slack_message, $slack_channel, $slack_agent, $slack_icon, $slack_color);
     break;
 
   case 'pantheon_deploy':
@@ -158,8 +154,8 @@ switch ($slack_type) {
     $slack_color = '#EFD01B';
     $slack_message = array();
     $slack_message['Deploy to Environment'] = '`' . $argv[2] . '`';
-    $slack_message['Message'] = 'Auto deploy of Wordpress updates (core, plugins)';
-    _slack_tell($slack_message, $slack_channel, $slack_agent, $slack_icon, $slack_color);
+    $slack_message['Message'] = 'Auto deploy of Drupal updates (core, modules)';
+    _slack_tell( $slack_message, $slack_channel, $slack_agent, $slack_icon, $slack_color);
     break;
 
   case 'pantheon_backup':
@@ -167,35 +163,36 @@ switch ($slack_type) {
     $slack_icon = 'https://d1qb2nb5cznatu.cloudfront.net/startups/i/907-cbdae2927a54d2281280f41e954e8a3d-medium_jpg.jpg';
     $slack_color = '#EFD01B';
     $slack_message = 'Creating a backup of the `live` environment.';
-    _slack_tell($slack_message, $slack_channel, $slack_agent, $slack_icon, $slack_color);
+    _slack_tell( $slack_message, $slack_channel, $slack_agent, $slack_icon, $slack_color);   
     break;
 
   case 'wizard_noupdates':
-    $slack_agent = 'Wordpress Update Wizard';
+    $slack_agent = 'Drupal Update Wizard';
     $slack_icon = '';
     $slack_color = '#666666';
     $slack_message = 'No new updates are found. Have a good day - http://framera.com/wp-content/uploads/2017/04/Have-a-Good-Day.jpg';
-    _slack_tell($slack_message, $slack_channel, $slack_agent, $slack_icon, $slack_color);
+    _slack_tell( $slack_message, $slack_channel, $slack_agent, $slack_icon, $slack_color);
     break;
 
   case 'wizard_updates':
-    $slack_agent = 'Wordpress Update Wizard';
-    $slack_icon = '';
+		$slack_agent = 'Drupal Update Wizard';
+		$slack_icon = '';
     $slack_color = '#666666';
     $slack_message = 'New updates are present and available for testing! Time to do this - https://media.giphy.com/media/12l061Wfv9RKes/giphy.gif';
-    _slack_tell($slack_message, $slack_channel, $slack_agent, $slack_icon, $slack_color);
+    _slack_tell( $slack_message, $slack_channel, $slack_agent, $slack_icon, $slack_color);
     break;
 
-  case 'wizard_done':
-    $slack_agent = 'Wordpress Update Wizard';
-    $slack_icon = '';
-    $slack_color = '#666666';
+	case 'wizard_done':
+		$slack_agent = 'Drupal Update Wizard';
+		$slack_icon = '';
+		$slack_color = '#666666';
 
-    // Post the File Using Uploads.IM.
+    // Post the File Using Uploads.IM
     $file_name_with_full_path = $argv[2];
     $response = \Cloudinary\Uploader::upload($file_name_with_full_path);
 
-    $slack_message = 'Your updates have been tested and applied. Enjoy your updated site! - ' . $response['secure_url'];
+    $slack_message = 'Your updates have been tested and applied. Enjoy your updated site! - '. $response['secure_url'];
     _slack_tell($slack_message, $slack_channel, $slack_agent, $slack_icon, $slack_color);
     break;
 }
+
